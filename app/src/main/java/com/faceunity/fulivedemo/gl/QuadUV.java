08d727c52a0610ce8a5785f4d5e23ae85b953e09
sujunjie <https://github.com/a1029453447/3399two-in-one.git>
2021-05-09 18:04:13 +0800

    optimized import.

--- app/src/main/java/com/faceunity/fulivedemo/gl/QuadUV.java
+++ app/src/main/java/com/faceunity/fulivedemo/gl/QuadUV.java
@@ -1,9 +1,7 @@
 package com.faceunity.fulivedemo.gl;
 
-import com.faceunity.fulivedemo.gl.GL2FloatBuffer;
-
-public class QuadUV extends GL2FloatBuffer {
-
+public class QuadUV extends GL2FloatBuffer
+{
     private static final float QuadUVData[] = {
             0.0f, 1.0f,
             1.0f, 1.0f,
--- app/src/main/java/com/faceunity/fulivedemo/renderer/DrawManager.java
+++ app/src/main/java/com/faceunity/fulivedemo/renderer/DrawManager.java
@@ -11,7 +11,6 @@ import com.faceunity.fulivedemo.drawobject.DrawObject;
 import com.faceunity.fulivedemo.drawobject.DrawText;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.Vector;
 
