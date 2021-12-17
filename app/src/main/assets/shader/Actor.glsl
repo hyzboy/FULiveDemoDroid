51c398c5e181f52adcb1f561461cbe603041a424
hyzboy <hyzboy@gmail.com>
2021-12-17 21:34:09 +0800

    更新人物去溢色算法

--- app/src/main/assets/shader/Actor.glsl
+++ app/src/main/assets/shader/Actor.glsl
@@ -7,16 +7,24 @@ float rgb2y(vec3 c)
     return (0.299*c.r + 0.587*c.g + 0.114*c.b);
 }
 
+vec4 clear_override_color(vec4 color)
+{
+    vec3 result=color.rgb*color.a;
+
+    float v = (2.0*result.b+result.r)/3.5;
+
+    if(result.g > v) result.g = mix(result.g,v,0.75);
+
+    float dif=rgb2y(color.rgb - result);
+
+    result += mix(0.0, dif, 0.3);
+
+    return vec4(result,color.a);
+}
+
 void main()
 {
     vec4 color=texture2D(sTexture, vTextureCoord);
 
-    vec3 result=color.rgb*color.a;
-    float v = (2.0*result.b+result.r)/3.5;
-    if(result.g > v) result.g = mix(result.g,v,0.75);
-    vec3 dif = (color.rgb - result);
-    float desaturatedDif = rgb2y(dif);
-    result += mix(0.0, desaturatedDif, 0.3);
-
-    gl_FragColor=vec4(result,color.a);
+    gl_FragColor=clear_override_color(color);
 }
